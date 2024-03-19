576f25a74630aede376a4533ea1c480c902f3fff
santi <santiago.montano@alten.es>
2024-03-19 12:36:25 +0100

    Primer commit implementación de función saludo

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,8 @@
 print("New Hello Git2!")
 print("Nueva impresión por pantalla")
 print("Nueva impresión por pantalla desde remoto")
+
+def saludo(nombre):
+    print("Hola "+ nombre+ "!")
+
+saludo("Santi")

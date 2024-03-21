ccb16d09a7b560054c1cea16e4f01a8641fd5a2c
santi <santiago.montano@alten.es>
2024-03-21 12:23:56 +0100

    Revert "Inclusión de nueva funcion"
    This reverts commit 8eef16e5fedb1eae1e55d3955281c7266ed466be.

--- hellogit.py
+++ hellogit.py
@@ -5,10 +5,6 @@ print("Nueva impresión por pantalla desde remoto")
 def saludo(nombre):
     print("Hola "+ nombre+ "!")
 
-def despedida(nombre):
-    print("Hasta luego "+ nombre)
-
 saludo("Santiago")
-despedida("Santiago")
 print("Rama master")
 print("Actualización commit de rama master")
